57ac540e20875b5f9905611457018a0d24fffba0
Harry-Kuria <harrisonkuria254@gmail.com>
2022-07-28 11:53:53 +0300

    commit message new

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import Footer from './Components/Home/Footer/Footer'
 import Home from './Components/Home/Home'
 import Services from './Components/Services/Services'
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
   <Routes>
-      
+      <Route exact path='/' element={<Home></Home>}></Route>
     
       <Route exact path='/services' element={<Services></Services>}></Route>
       <Route exact path='#contacts' element={<Footer id='contacts'></Footer>}></Route>
         
 
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
